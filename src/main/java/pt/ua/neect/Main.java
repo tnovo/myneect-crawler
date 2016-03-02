d12dd0df8ee34fa32ac568663eeca944289ad814
tnovo <t.novo.2@gmail.com>
2016-03-02 02:30:39 +0000

    User and pass as arguments

--- src/main/java/pt/ua/neect/Main.java
+++ src/main/java/pt/ua/neect/Main.java
@@ -22,8 +22,9 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        String user = "";
-        String pass = "";
+        String user = args[0];
+        String pass = args[1];
+       
         Crawler cr = new NeectCrawler("http://neect.ieeta.pt/forum/viewtopic.php?f=12&t=2534", user, pass);
         while (cr.hasNext()) {
             Document d = cr.next();
